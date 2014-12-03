eddc7e966071afb1c68ffc06353cd3034a21f1dd
chrjacobs <chr.jacobs@telenet.be>
2014-12-03 16:18:47 +0100

    eerste versie

---
+++ src/pkg8/school/School.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg8.school;
+
+/**
+ *
+ * @author Christel
+ */
+public class School {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        session session = HibernateUtil.getSessionFactory().openSession();
+//        session.beginTransaction();
+//
+//        session.persist();
+//        session.saveOrUpdate();
+//        session.delete();
+//
+//        session.createQuery session
+//        .getTransaction().commit();
+// 
+    }
+    
+}
